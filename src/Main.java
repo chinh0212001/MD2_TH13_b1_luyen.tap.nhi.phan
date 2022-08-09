2334e1a4f82d72ab5b8a778c505a1a2afad69e90
unknown <duongducchinh160@gmail.com>
2022-08-09 22:17:20 +0700

    tim kiem nhi phan ko su dunhg de quy

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b1 = 4;
+        int i1 = 123456;
+        long L1 = (long) i1;
+        short s2 = (short) i1;
+        byte b2 = (byte) b1;
+        int i2 = (int) 123.456;
+        long b3 = b1 + 7;
+    }
+}
